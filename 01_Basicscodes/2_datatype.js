87d120ba634d30ffccf417b2cdd1f849d0dfa668
Parth Sarthi Shukla <41308376+blackhatparth@users.noreply.github.com>
2024-07-22 00:46:00 +0530

    datatype done

---
+++ 01_Basicscodes/2_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // works in denojs not in nodejs because we are not using browser
+
+console.log(3+3); // code readability should be high
+
+console.log("Parth");
+
+let name = "Parth"
+let age = 24
+let isLoggedIn = true
+let state;
+
+// number => 2 to the power 53    range
+// bigint => used in stock market trading
+// string => ""
+// boolean => true/false
+// null => standalone value/representation of empty value
+// undefined => variable declared value not defined
+// symbol => unique
+
+
+
+console.log(typeof undefined); // output is undefined itself
+console.log(typeof null); // output is object // this is limitation of JS 
+
+
