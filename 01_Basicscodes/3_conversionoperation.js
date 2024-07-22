e4c6ac12cec2b0b269f48acdb0b7d6f6b452f6d4
Parth Sarthi Shukla <41308376+blackhatparth@users.noreply.github.com>
2024-07-22 19:49:32 +0530

    Operators done

--- 01_Basicscodes/3_conversionoperation.js
+++ 01_Basicscodes/3_conversionoperation.js
@@ -24,9 +24,56 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "parth" => true
 
 let anyNumber = 69
-console.log(typeof anyNumber);
-console.log(anyNumber);
+// console.log(typeof anyNumber);
+// console.log(anyNumber);
 
 let stringNumber = String(anyNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// **************** Operations **************** //
+
+let value = 45
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); // to find remainder // Modulus
+// console.log(2**2); // 2 rase to power 2 
+
+let str1 = "Hello"
+let str2 = " Parth"
+let  str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 3); // 13
+// console.log(1 + "3"); // 13
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((1 + 2) * 8 % 2); // write clean code not like that
+
+console.log(+true); // 1
+console.log(+""); //0
+
+let num1, num2, num3
+num1 =num2 = num3= 2+2 // readibility issue
+
+// ************ Prefix Operator ***********
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter); //incrementing value
+
+
+// *********** Postfix Operator ************
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter); //incrementing value
+
+// Read link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+/* Abstract Operation read link : https://dev.to/iphiee_oma/understand-javascript-abstract-operations-in-7-minutes-1951  /*
+
