ce8befbc854555d77c006ca7b9bcfeaef11075e9
Parth Sarthi Shukla <41308376+blackhatparth@users.noreply.github.com>
2024-07-22 16:23:50 +0530

    Conversion of Datatype

---
+++ 01_Basicscodes/3_conversionoperation.js
@@ -0,0 +1,32 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score)); // score inside method
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false 
+// "parth" => true
+
+let anyNumber = 69
+console.log(typeof anyNumber);
+console.log(anyNumber);
+
+let stringNumber = String(anyNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
